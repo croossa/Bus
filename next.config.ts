d234d37e980842c2bd23b8d830e8b8228acdd5e2
croossa <croossacom@gmail.com>
2026-01-05 11:58:46 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,28 +4,8 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
-
   reactCompiler: true,
   cacheComponents: true,
-
-  /* =========================
-     WWW → NON-WWW REDIRECT
-     ========================= */
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.croossa.com",
-          },
-        ],
-        destination: "https://croossa.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
